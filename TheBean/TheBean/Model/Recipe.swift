aace236715eab0268edff3d0bfc7d7b8c9763e5a
Daniel Pustotin <heartsker@gmail.com>
2022-02-04 12:54:26 +0300

    Recipe updated

--- TheBean/TheBean/Model/Recipe.swift
+++ TheBean/TheBean/Model/Recipe.swift
@@ -6,13 +6,16 @@
 //
 
 import Foundation
+import UIKit
 
 class Recipe {
     var title: String
     var info: String
+    var image: UIImage
     
-    init(_ title: String, _ info: String) {
+    init(_ title: String, _ info: String, img image: UIImage) {
         self.title = title
         self.info = info
+        self.image = image
     }
 }
